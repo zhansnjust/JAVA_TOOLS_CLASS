d8f363ed7e9fbf992c7917d201cfe9fd8442f75b
zhansnjust <841044399@qq.com>
2016-07-22 11:28:08 +0800

    7.22

--- java_learning/thread_learning/chapter_1/Main.java
+++ java_learning/thread_learning/chapter_1/Main.java
@@ -1,16 +1,31 @@
 package chapter_1;
 
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-		MyService service=new MyService();
-		ThreadA thread=new ThreadA(service);
-		thread.start();
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		Scanner sc=new Scanner(System.in);
+		while(sc.hasNext())
+		{
+			boolean flag=true;
+			int x=sc.nextInt();
+			String arr[]=new String[x];
+			for(int i=0;i<x;i++)
+				arr[i]=sc.next();
+			for(int i=0;i<x-1;i++)
+			{
+				if(arr[i].charAt(arr[i].length()-1)!=arr[i+1].charAt(0))
+				{
+					flag=false;
+					break;
+				}
+			}
+			if(flag==true)
+				System.out.println("Yes");
+			else
+				System.out.println("No");
+			
 		}
-		service.signal();
+		sc.close();
 	}
 }
